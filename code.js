5be92ed0b26bf9f2cc0bd6c91c6fac8ec18e8534
Aluno <moreiranicolas2008@gmail.com>
2025-02-17 08:35:17 -0300

    Primeiro

---
+++ code.js
@@ -0,0 +1,94 @@
+console.log("Exercício 1");
+console.log("");
+let nicolas = "Nicolas"
+console.log(nicolas);
+
+console.log("");
+
+console.log("Exercício 2");
+
+let num1 = 15
+let num2 = 15
+
+let soma = num1 + num2
+
+console.log(soma);
+
+console.log("");
+
+console.log("Exercício 3");
+
+var sim = "Sim "
+var nao = "Não"
+
+var uni = sim + nao
+
+console.log(uni);
+
+console.log("");
+
+console.log("Exercício 4");
+
+let a = 10
+let b = 20
+
+console.log("a:" + a);
+console.log("b:" + b);
+
+a = b
+b = a
+
+console.log("");
+
+console.log("a:" + a);
+console.log("b:" + b);
+
+console.log("");
+
+console.log("Exercício 5");
+
+let altura = 10
+let largura  = 6
+
+let area = altura * largura
+
+console.log("");
+console.log("area: " + area);
+
+console.log("");
+
+console.log("Exercício 6");
+
+let c = 20
+
+console.log("celsius: " + c);
+
+let F= (c*9/5 )+32
+
+console.log("Fahrenheit: " + f);
+
+console.log("");
+
+console.log("Exercício 7");
+
+let nume = 8
+console.log("Numero" + nume);
+
+let quadrado = nume * nume
+console.log("Ao quadrado" + quadrado);
+
+console.log("");
+
+console.log("Exercício 8");
+
+
+
+
+
+
+
+
+
+
+
+
