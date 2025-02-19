bda6a9a14b68240feb58b42454eebb7ed007ea99
Aluno <moreiranicolas2008@gmail.com>
2025-02-19 09:24:38 -0300

    s

---
+++ primo.js
@@ -0,0 +1,11 @@
+function encontrarPrimo(numero) {
+    if(nume%nume == 0 && nume%1 == 0){
+        return console.log("O numero é primo");
+    }else{
+        return console.log("Não é primo");
+    }
+}
+
+let num1 = 5
+
+console.log(encontrarPrimo(num1));
